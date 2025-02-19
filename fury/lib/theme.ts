c81c2c5fdb1da1906dd0092f83a61174a61da30d
nyel-101 <dandevjr@gmail.com>
2025-02-19 00:39:43 +0000

    style(auth): integrate middleware routes

--- fury/lib/theme.ts
+++ fury/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d983e808bfbc46d5a3b66d7fd5bae04e = () => {
-  return '2025-02-12T00:41:56Z';
+const node_15f4aac27b96499ebc58d292552eee69 = () => {
+  return '2025-02-19T00:39:43Z';
 };
