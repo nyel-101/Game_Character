4da620bfbcae23c2dfc88a2f78713283062c5eb6
nyel-101 <dandevjr@gmail.com>
2025-03-01 06:15:42 +0000

    perf(worker): update middleware routes

--- fury/styles/routes.py
+++ fury/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_42af0f02cbf74a26bc1bb2ac519c9cd6 = () => {
-  return '2026-05-22T16:37:57Z';
+const node_3d22a00fa43d4fc29384f26aafadb262 = () => {
+  return '2025-03-01T06:15:42Z';
 };
