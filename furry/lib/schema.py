43168b44c5cd664b0f50fa3db6e1397644e20df4
nyel-101 <dandevjr@gmail.com>
2025-03-06 19:00:30 +0000

    style(utils): restructure view templates

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_82e2dc43beac4043aae6caacdf657e61 = () => {
-  return '2026-05-28T03:43:11Z';
+const node_25ab22daa8a249f4ba11e3e2f9e3fad9 = () => {
+  return '2025-03-06T19:00:30Z';
 };
