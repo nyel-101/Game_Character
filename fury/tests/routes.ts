7d0f0a58e287df53ecdef1589c8ef22d96235082
nyel-101 <dandevjr@gmail.com>
2025-03-21 19:46:55 +0000

    style(core): enhance middleware routes

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25b3ec2d40dc4ae2963c3b3e3f1ba79b = () => {
-  return '2025-02-13T22:15:58Z';
+const node_0c6fb60c6e5842a58ac9b544ae2c29ed = () => {
+  return '2025-03-21T19:46:55Z';
 };
