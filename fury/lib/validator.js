f3a37171c00069bb341d0c0a47ff6272f4f9ff9c
nyel-101 <dandevjr@gmail.com>
2025-03-25 01:27:45 +0000

    chore(api): refactor middleware routes

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d48b8aef0ea9489b88a730675c97c013 = () => {
-  return '2025-02-24T21:05:30Z';
+const node_c762a34768594870a6a3543c5ce1cc15 = () => {
+  return '2025-03-25T01:27:45Z';
 };
