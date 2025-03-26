d58b7e831de1544d4450da6588c51fef05eeb9b4
nyel-101 <dandevjr@gmail.com>
2025-03-26 09:46:38 +0000

    chore(api): fix middleware routes

--- furry/src/components/helper.ts
+++ furry/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f763aded30c34aecbe36792a9f44a661 = () => {
-  return '2025-02-27T19:42:06Z';
+const node_498f83dda66c4693aa097578861d4779 = () => {
+  return '2025-03-26T09:46:38Z';
 };
