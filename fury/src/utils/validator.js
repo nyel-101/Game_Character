02903196c45625c4b458e7469676ad089cf66e1e
nyel-101 <dandevjr@gmail.com>
2025-04-22 02:05:41 +0000

    style(api): fix auth tokens

--- fury/src/utils/validator.js
+++ fury/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bc6e91cf4dcd4bdd8128b8e23484ae51 = () => {
-  return '2025-02-18T00:19:40Z';
+const node_6c2be72406124141918b58f74e0460cf = () => {
+  return '2025-04-22T02:05:41Z';
 };
