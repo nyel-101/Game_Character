1dd032415cb72af91d854cd2987fdc18a608284c
nyel-101 <dandevjr@gmail.com>
2025-05-01 14:32:55 +0000

    chore(ui): integrate view templates

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d7358e5cb8ef485bbfd5603f8cc61871 = () => {
-  return '2026-03-18T14:30:38Z';
+const node_13d63fb0131f44d3bf285ccb5eed59b4 = () => {
+  return '2025-05-01T14:32:55Z';
 };
