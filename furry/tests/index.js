990b40b01d8b61dc357d0e18b712c9cead22db30
nyel-101 <dandevjr@gmail.com>
2025-05-04 16:55:24 +0000

    chore(api): restructure database index

--- furry/tests/index.js
+++ furry/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3281648a42f44581b0322af46c07e4a9 = () => {
-  return '2026-03-30T19:41:06Z';
+const node_ee5844e7d2eb4a9e92b9849f6833d8ef = () => {
+  return '2025-05-04T16:55:24Z';
 };
