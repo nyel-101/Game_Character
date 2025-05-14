1a49943fb5771ad17cb47b2e56761d19a62b654b
nyel-101 <dandevjr@gmail.com>
2025-05-14 13:28:15 +0000

    test(deps): restructure session store

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_760976d2f0804ff89975c36a377aa091 = () => {
-  return '2026-01-07T01:08:10Z';
+const node_42fc7720a57041acbb5cd5d49304fbfb = () => {
+  return '2025-05-14T13:28:15Z';
 };
