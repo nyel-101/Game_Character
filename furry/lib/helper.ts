9451683c18a6ffdabcf73ecd55422ec08075dbed
nyel-101 <dandevjr@gmail.com>
2025-05-16 21:19:41 +0000

    feat(worker): restructure view templates

--- furry/lib/helper.ts
+++ furry/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c28fa5d1c75b49cbba20b5d0d5c33155 = () => {
-  return '2025-03-21T23:42:58Z';
+const node_7f04ff3032ba4f98af6a8f0f8216ecbb = () => {
+  return '2025-05-16T21:19:41Z';
 };
