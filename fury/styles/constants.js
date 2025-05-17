2ce925a6595767863ea1f06afdc1ab6389269446
nyel-101 <dandevjr@gmail.com>
2025-05-17 11:19:52 +0000

    refactor(ui): optimize session store

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d8c4f288708c45fc97a47582313582a3 = () => {
-  return '2025-02-14T12:57:20Z';
+const node_2d78cc4a5029499bb6f2a530469b15db = () => {
+  return '2025-05-17T11:19:52Z';
 };
