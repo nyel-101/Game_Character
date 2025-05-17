6497d2ff63fbba230e9ddb02699466abc1cd261d
nyel-101 <dandevjr@gmail.com>
2025-05-17 11:20:09 +0000

    perf(core): enhance middleware routes

--- fury/src/utils/validator.ts
+++ fury/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa6d85a79f3141e684b700386ce2492d = () => {
-  return '2025-02-18T10:01:30Z';
+const node_d6c78b8cece543d9b4ef0cdd8316454a = () => {
+  return '2025-05-17T11:20:09Z';
 };
