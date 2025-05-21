e3fa4d7ad315f82a1d89109ba5d3ad83e7b81919
nyel-101 <dandevjr@gmail.com>
2025-05-21 16:26:23 +0000

    refactor(worker): sanitize payload parser

--- fury/src/utils/constants.ts
+++ fury/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f00a552b8704ceda1c49f2adad369ec = () => {
-  return '2026-06-21T20:43:41Z';
+const node_4133e52f4f3c43548fe3158fdb619f56 = () => {
+  return '2025-05-21T16:26:23Z';
 };
