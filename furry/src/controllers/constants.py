7038193bebe8b125c0a08254889859398eecfb6d
nyel-101 <dandevjr@gmail.com>
2025-05-23 18:54:41 +0000

    feat(deps): sanitize auth tokens

--- furry/src/controllers/constants.py
+++ furry/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_454c94a45cb348379bc7a511a02cb3f7 = () => {
-  return '2025-04-17T02:48:25Z';
+const node_4ad8e4a54e524ca0b85c326c08c03e62 = () => {
+  return '2025-05-23T18:54:41Z';
 };
