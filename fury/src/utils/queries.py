56796919feab6198711ffcda2fa909891a113853
nyel-101 <dandevjr@gmail.com>
2025-05-28 06:44:42 +0000

    perf(auth): optimize database index

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0ec5fb1b25194e25b3faa6518bb1c841 = () => {
-  return '2025-04-29T08:57:59Z';
+const node_c18d08296eda45869dee93af043a9e72 = () => {
+  return '2025-05-28T06:44:42Z';
 };
