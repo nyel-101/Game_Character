1e9309af6aff63ac70eaa9dc8962568cbd0657bd
nyel-101 <dandevjr@gmail.com>
2025-05-29 22:50:00 +0000

    test(db): sanitize auth tokens

--- furry/lib/routes.py
+++ furry/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ddd48a8856ac408ea0569f274780892f = () => {
-  return '2026-02-28T20:25:16Z';
+const node_dff7bf411388473da25c81467902185b = () => {
+  return '2025-05-29T22:50:00Z';
 };
