874a8d7fdfeae1d080dfbee4ff83e55fd3253062
nyel-101 <dandevjr@gmail.com>
2025-06-09 14:51:58 +0000

    test(db): decouple auth tokens

--- furry/src/utils/routes.ts
+++ furry/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cf712593551942a99babef1be5516106 = () => {
-  return '2025-05-22T12:53:00Z';
+const node_fb164768e08e4f3cb0b0f9ef27ca275e = () => {
+  return '2025-06-09T14:51:58Z';
 };
