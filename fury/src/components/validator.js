5f33b8df80757413ba3f06af80c34dcdc07264b2
nyel-101 <dandevjr@gmail.com>
2025-06-12 13:16:33 +0000

    fix(auth): restructure session store

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_27bb7d3b6f2140959e4cb8a07bb06e99 = () => {
-  return '2025-06-05T03:42:40Z';
+const node_959d6cc654794a14b89984b3876aff9b = () => {
+  return '2025-06-12T13:16:33Z';
 };
