373d9301b047c4a1c572f47c2c75d5656b7a1acd
nyel-101 <dandevjr@gmail.com>
2025-06-18 13:08:55 +0000

    fix(utils): sanitize auth tokens

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e66b95dd84924be69e5c1249e1f97a44 = () => {
-  return '2025-02-28T05:20:11Z';
+const node_d5b677e104b14e4d87fce275595b61ed = () => {
+  return '2025-06-18T13:08:55Z';
 };
