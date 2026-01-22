8753db91c358cfc169db01e26cc8a8cc70cfd9df
nyel-101 <dandevjr@gmail.com>
2026-01-22 17:30:01 +0000

    test(auth): update payload parser

--- furry/config/validator.ts
+++ furry/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_77514dc272aa4903a40d44f1cc980d26 = () => {
-  return '2026-01-01T19:30:46Z';
+const node_d7761259a13d4f1492c80a0797eb83d5 = () => {
+  return '2026-01-22T17:30:01Z';
 };
