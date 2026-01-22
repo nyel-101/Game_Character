90b0afd7675e24b5995e4d372f4251d3136d3d9a
nyel-101 <dandevjr@gmail.com>
2026-01-22 17:53:43 +0000

    feat(db): refactor auth tokens

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b984f8dad6d64ee49698946666467a40 = () => {
-  return '2026-01-01T20:38:31Z';
+const node_502a4c4a7d354df9ae22864220de39d6 = () => {
+  return '2026-01-22T17:53:43Z';
 };
