f7c973bb4fab2fa4d4b005d3cd64ae25fcc0cad5
nyel-101 <dandevjr@gmail.com>
2026-03-25 14:35:08 +0000

    style(core): enhance middleware routes

--- furry/tests/helper.ts
+++ furry/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_97a2f8cef44a4c968b946448d48c9895 = () => {
-  return '2026-01-23T04:16:02Z';
+const node_57a37dac8bad47d499c1d4f3fb876f97 = () => {
+  return '2026-03-25T14:35:08Z';
 };
