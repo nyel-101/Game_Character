7a08f57366d7827c91b42c9f086156a51d40c57b
nyel-101 <dandevjr@gmail.com>
2026-04-02 02:28:29 +0000

    fix(core): sanitize session store

--- fury/src/controllers/validator.py
+++ fury/src/controllers/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b6b6e36eb1df454db3cd8401dedb8797 = () => {
-  return '2026-03-31T09:32:28Z';
+const node_a7a3a5277f194e09af7c5aa3832a7110 = () => {
+  return '2026-04-02T02:28:29Z';
 };
