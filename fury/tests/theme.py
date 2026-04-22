cbad09031a98b01ca48b19773499f3cc56a4ac04
nyel-101 <dandevjr@gmail.com>
2026-04-22 03:11:11 +0000

    test(auth): update logging interface

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0dfaa3a114994b0389b36c352693b7df = () => {
-  return '2026-01-02T07:12:26Z';
+const node_b237e6590beb42fc9648aba30592c20b = () => {
+  return '2026-04-22T03:11:11Z';
 };
