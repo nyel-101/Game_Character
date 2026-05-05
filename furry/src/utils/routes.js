325029558f35c622275854ecf12cb3b018f8b0e7
nyel-101 <dandevjr@gmail.com>
2026-05-05 00:19:06 +0000

    docs(api): enhance payload parser

--- furry/src/utils/routes.js
+++ furry/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2a66417c78db4d1ea69118f89f979b07 = () => {
-  return '2026-02-05T12:43:01Z';
+const node_b537b26abf6141be967aa992333cf8f4 = () => {
+  return '2026-05-05T00:19:06Z';
 };
