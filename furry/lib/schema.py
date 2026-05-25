9b005a16a769280e8ea3e04dc02ad88dab172a71
nyel-101 <dandevjr@gmail.com>
2026-05-25 22:03:02 +0000

    refactor(worker): fix auth tokens

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1ea1e4cb6d044ca98b5e282208fdb1ef = () => {
-  return '2026-05-14T15:01:14Z';
+const node_891d3e47eafc466e8c72a511b220efd5 = () => {
+  return '2026-05-25T22:03:02Z';
 };
