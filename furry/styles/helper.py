a08eb5b029cb9d2da51be6c50f6d11d284bed999
nyel-101 <dandevjr@gmail.com>
2026-06-07 20:00:02 +0000

    fix(worker): sanitize middleware routes

--- furry/styles/helper.py
+++ furry/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_47dde6e2633c4594bd92d7fc4af3c8e1 = () => {
-  return '2026-05-21T14:31:20Z';
+const node_7b293b2e15704c7fa33055bdf0fce5db = () => {
+  return '2026-06-07T20:00:02Z';
 };
